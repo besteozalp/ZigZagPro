b04febaae6a25994908df609bc04230639aae49a
Beste ÖZALP <31745124+besteozalp@users.noreply.github.com>
2019-01-08 20:31:02 +0300

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public bool isGameStarted;
+	// Use this for initialization
+	void Start () {
+        isGameStarted = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            StartGame();
+        }
+		
+	}
+
+    private void StartGame()
+    {
+        isGameStarted = true;
+    }
+
+    internal void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+}

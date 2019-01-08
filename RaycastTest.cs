d7635e192ec62da5c3f4eecbbdeb66374d6f63a6
Beste ÖZALP <31745124+besteozalp@users.noreply.github.com>
2019-01-08 20:31:41 +0300

    Add files via upload

---
+++ RaycastTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Ray ray = new Ray(transform.position, Vector3.down);
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            hitInfo.transform.name = "x";
+            Debug.Log("asagida " + hitInfo.transform.name + " var");
+        }
+
+		
+	}
+}

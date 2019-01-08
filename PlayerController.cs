077ddd4f0036b2ee01edad786acd16ee94739dc9
Beste ÖZALP <31745124+besteozalp@users.noreply.github.com>
2019-01-08 20:30:41 +0300

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    bool sagaMiBakiyor;
+    public Rigidbody rb;
+    GameManager gameManager;
+    Animator animController;
+    public Transform rayOrijin;
+     
+
+    private void Start()
+    {
+        sagaMiBakiyor = true;
+        gameManager = FindObjectOfType<GameManager>();
+        animController = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            YonDegistir();
+        }
+
+        if (transform.position.y < -2)
+        {
+            gameManager.RestartGame();
+        }
+
+	}
+
+    private void YonDegistir()
+    {
+        sagaMiBakiyor = !sagaMiBakiyor;
+
+        if (sagaMiBakiyor)
+        {
+            transform.rotation = Quaternion.Euler(0, 45, 0);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, -45, 0);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!gameManager.isGameStarted) return;
+        else
+        {
+            animController.SetTrigger("gameStarted");
+            rb.position += transform.forward * Time.deltaTime;
+
+            if (!Physics.Raycast(rayOrijin.position, Vector3.down))
+            {
+                animController.SetTrigger("falling");
+            }
+        }
+    }
+}
